81a8646072232d9f0c74a033468778705d405522
mnoskov <mnoskov@naumen.ru>
2020-12-26 22:30:47 +0500

    optimize output, input, command to constants

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,32 +1,52 @@
 package main;
 
-import java.util.Scanner;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 import calculatorReversePolishNotation.Calculator;
 
 public class Main
 {
+    private static final String FIRST_MSG =
+            "Enter an expression in the reverse Polish notation." + System.lineSeparator()
+                    + "To exit, enter close()." + System.lineSeparator();
+    private static final String ERROR_MSG = "Sorry, I don't understand." + System.lineSeparator();
+    private static final String CLOSE_COMMAND = "close()";
+
     public static void main(String[] args)
     {
         Calculator calculator = new Calculator();
         String input;
-        Scanner sc = new Scanner(System.in);
-
-        while (true)
+        try (var bw = new BufferedOutputStream(System.out);
+             var br = new BufferedReader(new InputStreamReader(System.in)))
         {
-            System.out.println("Enter an expression in the reverse Polish notation");
-            System.out.println("To exit, enter close()");
-            input = sc.nextLine();
-            if (input.equals("close()"))
-                break;
-            try
+            while (true)
             {
-                System.out.println(calculator.calculate(input));
+                bw.write(FIRST_MSG.getBytes(StandardCharsets.UTF_8));
+                bw.flush();
+                input = br.readLine();
+                if (input.equals(CLOSE_COMMAND))
+                {
+                    break;
+                }
+                try
+                {
+                    bw.write((calculator.calculate(input) + System.lineSeparator()).getBytes(StandardCharsets.UTF_8));
+                    bw.flush();
+                }
+                catch (Exception e)
+                {
+                    bw.write((ERROR_MSG).getBytes(StandardCharsets.UTF_8));
+                    bw.flush();
+                }
             }
-            catch (Exception e)
-            {
-                System.out.println("sorry, I don't understand");
-            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
         }
     }
 }
